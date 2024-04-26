b5b6e261260bb2cb849bf9399ef1a4e42d0a8410
paulo aug <124801497+pauloaug07@users.noreply.github.com>
2024-04-26 12:45:55 -0300

    Add files via upload

---
+++ exe02/exe02/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace exe02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ exe02/exe02/RegistroDeCompras.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exe02
+{
+    public class RegistroDeCompras
+    {
+        private string produto;
+        private double valor;
+        private DateTime data;
+
+        public RegistroDeCompras(string produto, double valor, DateTime data)
+        {
+            this.produto = produto;
+            this.valor = valor;
+            this.data = data;
+        }
+    }
+}
