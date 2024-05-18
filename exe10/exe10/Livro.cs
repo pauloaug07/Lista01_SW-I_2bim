8ce3d76376efe57c1fec31a41c5624ace1be34d9
paulo aug <124801497+pauloaug07@users.noreply.github.com>
2024-05-17 22:08:58 -0300

    Add files via upload

---
+++ exe10/exe10/Livro.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exe10
+{
+    class Animal
+    {
+        public string Nome { get; set; }
+        public string Especie { get; set; }
+        public int Idade { get; set; }
+        public string Som { get; set; }
+
+        public Animal(string nome, string especie, int idade, string som)
+        {
+            Nome = nome;
+            Especie = especie;
+            Idade = idade;
+            Som = som;
+        }
+
+        public void EmitirSom()
+        {
+            Console.WriteLine($"O {Especie} {Nome} emite o som: {Som}");
+        }
+    }
+}
+
---
+++ exe10/exe10/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace exe10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Criando um animal
+            Animal animal = new Animal("Rex", "Cachorro", 3, "Au Au!");
+
+            // Emitindo o som do animal
+            animal.EmitirSom(); // Saída: O Cachorro Rex emite o som: Au Au!
+
+        }
+    }
+}
