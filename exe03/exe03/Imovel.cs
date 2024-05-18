93790d140c19afb5bdedf84233b91ae6793114bf
paulo aug <124801497+pauloaug07@users.noreply.github.com>
2024-05-17 21:53:43 -0300

    Add files via upload

---
+++ exe03/exe03/Imovel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exe03
+{
+    class Imovel
+    {
+        private string Endereco { get; set; }
+        private double Preco { get; set; }
+        private string Tipo { get; set; }
+
+        public Imovel(string endereco, double preco, string tipo)
+        {
+            Endereco = endereco;
+            Preco = preco;
+            Tipo = tipo;
+        }
+
+        public string GetEndereco()
+        {
+            return Endereco;
+        }
+
+        public double GetPreco()
+        {
+            return Preco;
+        }
+
+        public string GetTipo()
+        {
+            return Tipo;
+        }
+
+        public void AlterarPreco(double novoPreco)
+        {
+            Preco = novoPreco;
+        }
+    }
+}
