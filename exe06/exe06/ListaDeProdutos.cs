b00562f04be38d06d7af0295bb9206741b76cc0d
paulo aug <124801497+pauloaug07@users.noreply.github.com>
2024-05-17 22:00:31 -0300

    Add files via upload

---
+++ exe06/exe06/ListaDeProdutos.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exe06
+{
+    class ListaDeProdutos
+    {
+        private List<Produto> produtos;
+
+        public ListaDeProdutos()
+        {
+            produtos = new List<Produto>();
+        }
+
+        public void AdicionarProduto(Produto produto)
+        {
+            produtos.Add(produto);
+        }
+
+        public void ListarProdutos()
+        {
+            Console.WriteLine("Lista de Produtos:");
+            foreach (var produto in produtos)
+            {
+                produto.MostrarInformacoes();
+            }
+        }
+
+        public Produto BuscarProduto(string nome)
+        {
+            foreach (var produto in produtos)
+            {
+                if (produto.GetNome() == nome)
+                {
+                    return produto;
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ exe06/exe06/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace exe06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ListaDeProdutos listaDeProdutos = new ListaDeProdutos();
+
+            Produto produto1 = new Produto("Camiseta", 29.99, 50);
+            Produto produto2 = new Produto("Calça Jeans", 59.99, 30);
+            Produto produto3 = new Produto("Tênis", 99.99, 20);
+
+            listaDeProdutos.AdicionarProduto(produto1);
+            listaDeProdutos.AdicionarProduto(produto2);
+            listaDeProdutos.AdicionarProduto(produto3);
+
+            listaDeProdutos.ListarProdutos();
+
+            string nomeParaBuscar = "Calça Jeans";
+            Produto produtoConsultado = listaDeProdutos.BuscarProduto(nomeParaBuscar);
+
+            if (produtoConsultado != null)
+            {
+                Console.WriteLine($"Produto encontrado com sucesso: {nomeParaBuscar}");
+            }
+            else
+            {
+                Console.WriteLine($"Produto com o nome {nomeParaBuscar} não encontrado.");
+            }
+        }
+    }
+}
